a6b7b3b8a71545e5ae15057a28b84348f40d26dc
AR Janabi <aaljanabi1992@gmail.com>
2023-05-16 12:10:41 -0700

    building a list of products using useContext hook

---
+++ src/context/products.context.jsx
@@ -0,0 +1,12 @@
+import { createContext, useContext, useState } from "react";
+import PRODUCTS from '../shop-data.json';
+
+export const ProductsContext = createContext({
+    products: [],
+});
+
+export const ProductProvider = ({ children }) => {
+    const [products, setProducts] = useState(PRODUCTS);
+    const value = { products };
+    return (<ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>)
+}
